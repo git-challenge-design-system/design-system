778d38c26d9e780320433b256cd86c444f862200
CHAE <dbscogus4467@naver.com>
2024-01-23 00:01:56 +0900

    config: 스토리북 배포
    Co-Authored-By: 박유현 <puh0128@gmail.com>

---
+++ src/components/Button/Button.css.ts
@@ -0,0 +1,92 @@
+import { style, styleVariants } from "@vanilla-extract/css";
+
+import color from "../../tokens/color";
+import typography from "../../tokens/typography";
+import { borderRadius } from "../../tokens/utils.css";
+
+export const buttonBase = style([
+  typography.$semantic.title4Regular,
+  borderRadius,
+  {
+    height: 42,
+    border: "1px solid transparent",
+    padding: "8px 13px",
+
+    ":disabled": {
+      borderColor: color.$semantic.bgDisabled,
+      color: color.$semantic.textDisabled,
+      backgroundColor: color.$semantic.bgDisabled,
+    },
+  },
+]);
+
+export const buttonVariants = styleVariants({
+  primaryFill: {
+    color: color.$semantic.textWhite,
+    backgroundColor: color.$semantic.primary,
+
+    selectors: {
+      "&:hover:not(:disabled)": {
+        backgroundColor: color.$semantic.primaryHover,
+      },
+    },
+  },
+
+  secondaryFill: {
+    color: color.$semantic.textWhite,
+    backgroundColor: color.$semantic.secondary,
+
+    selectors: {
+      "&:hover:not(:disabled)": {
+        backgroundColor: color.$semantic.secondaryHover,
+      },
+    },
+  },
+
+  primaryLine: {
+    border: `1px solid ${color.$semantic.primary}`,
+    color: color.$semantic.primary,
+    backgroundColor: color.$semantic.bgWhite,
+
+    selectors: {
+      "&:hover:not(:disabled)": {
+        backgroundColor: color.$semantic.primaryLow,
+      },
+    },
+  },
+
+  secondaryLine: {
+    border: `1px solid ${color.$semantic.secondary}`,
+    color: color.$semantic.secondary,
+    backgroundColor: color.$semantic.bgWhite,
+
+    selectors: {
+      "&:hover:not(:disabled)": {
+        color: color.$semantic.secondary,
+        backgroundColor: color.$semantic.secondaryLow,
+      },
+    },
+  },
+
+  primaryLow: {
+    color: color.$semantic.primary,
+    backgroundColor: color.$semantic.primaryLow,
+
+    selectors: {
+      "&:hover:not(:disabled)": {
+        backgroundColor: color.$semantic.primaryLowHover,
+      },
+    },
+  },
+
+  secondaryLow: {
+    color: color.$semantic.secondary,
+    backgroundColor: color.$semantic.secondaryLow,
+
+    selectors: {
+      "&:hover:not(:disabled)": {
+        backgroundColor: color.$semantic.secondaryLowHover,
+      },
+    },
+  },
+});
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,47 @@
+import type { ButtonHTMLAttributes, ReactNode } from "react";
+
+import { widthFull } from "../../tokens/utils.css";
+import classnames from "../../utils/classnames";
+
+import * as styles from "./Button.css";
+
+export type ButtonVariantType = keyof typeof styles.buttonVariants;
+
+export interface ButtonProps
+  extends Pick<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    "type" | "disabled" | "onClick"
+  > {
+  full?: boolean;
+  variant: ButtonVariantType;
+  children: ReactNode;
+  className?: string;
+}
+
+export function Button({
+  full = false,
+  variant,
+  children,
+  type = "button",
+  disabled = false,
+  onClick,
+  className = "",
+}: ButtonProps) {
+  const buttonStyle = classnames(
+    styles.buttonBase,
+    styles.buttonVariants[variant],
+    full ? widthFull : "",
+    className,
+  );
+
+  return (
+    <button
+      type={type === "button" ? "button" : "submit"}
+      className={buttonStyle}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
