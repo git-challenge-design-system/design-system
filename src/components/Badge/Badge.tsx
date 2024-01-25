3cbed5e0613c3e9e9fa843807b65a220a49b5f3a
CHAE <dbscogus4467@naver.com>
2024-01-25 16:32:12 +0900

    docs: Badge 컴포넌트 문서화

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -9,12 +9,15 @@ import * as styles from "./Badge.css";
 export type BadgeVariantType = keyof typeof badgeVariants;
 
 export interface BadgeProps {
+  /** Badge의 스타일을 결정합니다. */
   variant: BadgeVariantType;
+  /** Badge안에 내용이 들어갑니다. */
   children: ReactNode;
 }
 
 export const badgeVariantList = objectKeys(badgeVariants);
 
+/** Badge는 빠른 인식을 위해 항목의 상태를 강조하기 위해 사용되는 컴포넌트입니다. */
 export function Badge({ variant, children }: BadgeProps) {
   const badgeStyle = classnames(
     styles.badgeBase,
