ac64dce831a5aa72542283c4bc8dd28c7c562644
YuHyun <puh0128@gmail.com>
2024-01-31 22:56:59 +0900

    docs: Accordion 컴포넌트 문서화

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -5,11 +5,20 @@ import AccordionDetails from "./AccordionDetails";
 import AccordionSummary from "./AccordionSummary";
 
 export interface AccordionProps {
+  /**
+   * Accordion의 너비를 결정합니다.
+   * @default 200
+   */
   width?: number | string;
+  /**
+   * Accordion의 초기 열림 상태를 결정합니다.
+   * @default false
+   */
   open?: boolean;
   children: ReactNode;
 }
 
+/** Accordion은 세부 내용을 숨기거나 보여줄 수 있는 컴포넌트입니다. */
 function Accordion({
   width = 200,
   open: initOpen = false,
