11e0c7227033e795fd39a610f333f061f33538ba
CHAE <dbscogus4467@naver.com>
2024-02-20 15:09:34 +0900

    docs: CodeBlock 컴포넌트 문서화

--- src/components/CodeBlock/CodeBlock.tsx
+++ src/components/CodeBlock/CodeBlock.tsx
@@ -1,10 +1,12 @@
 import { container } from "./CodeBlock.css";
 
 interface CodeBlockProps {
+  /** 코드 블록 내부 코드의 내용입니다. 배열 요소별로 줄바꿈이 적용됩니다. */
   code: string[];
   className?: string;
 }
 
+/** CodeBlock은 코드 블록의 용도로 사용되는 컴포넌트입니다. 코드 블록 요소 내부에 있는 백틱(``)으로 감싸진 부분에 코드 하이라이팅이 적용됩니다.  */
 export function CodeBlock({ code, className = "" }: CodeBlockProps) {
   return (
     <p
